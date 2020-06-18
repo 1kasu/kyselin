1ac65e5f79184cd4e5f8b3c7375750c5851d15c9
Kasimir Jyry Santeri Ilmonen <jyrysanteri@hotmail.com>
2020-06-18 16:10:46 +0300

    Alustettu yhtÃ¤kyselyriviÃ¤ vastaavaa tyyppiÃ¤

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::collections::HashMap;
+
+type Kysyttava = HashMap<String, String>;
+
+
 fn main() {
     println!("Hello, world!");
+    let mut a = Kysyttava::new(); 
+    a.insert("perusmuoto".to_string(),"食べる".to_string());
 }
+/*
+struct Kysyttava {
+    pub kysyttava: HashMap<String, String>
+}
+
+impl Kysyttava {
+    pub fn new() -> Kysyttava {
+        Kysyttava {
+            kysyttava: HashMap::new()
+        }
+    }
+}
+*/
